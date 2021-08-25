a9346a84113f6cff3366403f9ef9ad0fb96f49f8
dante <are2341@nate.com>
2021-08-26 01:44:58 +0900

    no message

--- Framework/WinAPI/Example/Example/Classes/Example_05/Example_05.cpp
+++
@@ -1,7 +0,0 @@
-#include "Example_05.h"
-
-Example_05::Example_05(HINSTANCE a_hInst, int a_nShowOpts, const SIZE & a_rstWndSize) 
-: CWndApp(a_hInst, a_nShowOpts, a_rstWndSize) 
-{
-	// Do Nothing
-}
